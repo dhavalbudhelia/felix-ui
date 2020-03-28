720a1bafe07dbc011c09493c1d6d71a6589ed1bf
Dhaval Budhelia <dhaval@supportability.com.au>
2020-03-28 17:38:02 +1000

    fix for extra nesting in options

--- src/utils/options.js
+++ src/utils/options.js
@@ -1,11 +1,13 @@
 const optionsDefaults = {
-    color: {
-        primary: 'indigo-500',
-        primaryDark: 'indigo-600',
-        secondary: 'indigo-100',
-        secondaryDark: 'indigo-200',
-        tertiary: 'gray-300',
-        tertiaryDark: 'gray-400',
+    options: {
+        color: {
+            primary: 'indigo-500',
+            primaryDark: 'indigo-600',
+            secondary: 'indigo-100',
+            secondaryDark: 'indigo-200',
+            tertiary: 'gray-300',
+            tertiaryDark: 'gray-400',
+        },
     },
 };
 
