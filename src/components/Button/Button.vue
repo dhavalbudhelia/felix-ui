04131d0f9370811fcbf92b1649ebf85b38428cee
Dhaval Budhelia <dhaval@supportability.com.au>
2020-03-28 17:14:56 +1000

    Button: moving scss style to the component

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,19 +1,21 @@
 <template>
-    <button type="button" :class="[classObject, size, cssClass]" :disabled="disabled" @click="emitClick">
+    <button type="button" :class="[classObject, cssClass]" :disabled="disabled" @click="emitClick">
         <template v-if="!iconOnly">
-            <span v-if="iconBefore" class="icon">
-                <Icon :icon-pack="iconPackBefore" :icon="iconBefore" :size="size"></Icon>
-            </span>
-            <span><slot></slot></span>
-            <span v-if="iconAfter" class="icon">
-                <Icon :icon-pack="iconPackAfter" :icon="iconAfter" :size="size"></Icon>
-            </span>
+            <Icon v-if="iconBefore"
+                  :class="iconClass"
+                  :icon-pack="iconPackBefore"
+                  :icon="iconBefore"
+                  :size="size"
+            ></Icon>
+            <div class="mx-2"><slot></slot></div>
+            <Icon v-if="iconAfter"
+                  :class="iconClass"
+                  :icon-pack="iconPackAfter"
+                  :icon="iconAfter"
+                  :size="size"
+            ></Icon>
         </template>
-        <template v-else>
-            <span class="icon">
-                <Icon :icon-pack="iconPack" :icon="icon" :size="size"></Icon>
-            </span>
-        </template>
+        <Icon v-else :class="iconClass" :icon-pack="iconPack" :icon="icon" :size="size"></Icon>
     </button>
 </template>
 
@@ -21,6 +23,8 @@
     import Icon from '@/components/Icon/Icon';
     import IconMixin from "@/mixins/IconMixin";
     import SizeMixin from "@/mixins/SizeMixin";
+    import optionsDefaults from '@/utils/options';
+    import CssClasses from "./CssClasses";
 
     export default {
         name: 'fe-button',
@@ -45,19 +49,76 @@
                 type: String,
                 required: false,
                 default: ''
-            }
+            },
+            options: {
+                type: Object,
+                required: false,
+                default: function () {
+                    return optionsDefaults;
+                },
+            },
         },
         computed: {
             /**
              * class object
              */
             classObject() {
-                return {
-                    'fe-button': true,
-                    'inverted': this.inverted,
-                    'plain': this.plain,
-                };
-            }
+                let classes =  ['flex items-center', CssClasses.base];
+                if (this.inverted) {
+                    classes.push(CssClasses.inverted);
+                } else if (this.plain) {
+                    classes.push(CssClasses.plain);
+                } else if (this.disabled) {
+                    classes.push(CssClasses.disabled);
+                } else {
+                    classes.push(CssClasses.general);
+                }
+                classes.push(this.sizeClass);
+                classes.push(this.colorClass);
+                return classes;
+            },
+            /**
+             * size class object
+             */
+            sizeClass() {
+                switch (this.size) {
+                    case 'is-xs':
+                        return CssClasses.sizeXs;
+                    case 'is-sm':
+                        return CssClasses.sizeSm;
+                    case 'is-md':
+                        return CssClasses.sizeMd;
+                    case 'is-lg':
+                        return CssClasses.sizeLg;
+                    default:
+                        return CssClasses.sizeMd;
+                }
+            },
+            /**
+             * color class object
+             */
+            colorClass() {
+                let primary = `bg-${this.options.color.primary}`;
+                let primaryDark = `bg-${this.options.color.primaryDark}`;
+                if (this.inverted) {
+                    primary = `bg-white`;
+                    let darkInverted = `bg-${this.options.color.tertiaryDark}`;
+                    primaryDark = `hover:${darkInverted} active:${darkInverted} focus:${darkInverted}`;
+                } else if (this.plain) {
+                    primary = `bg-white`;
+                    primaryDark = `hover:bg-white active:bg-white focus:bg-white`;
+                } else if (this.disabled) {
+                    primary = `bg-${this.options.color.tertiary}`;
+                    primaryDark = `bg-${this.options.color.tertiary}`;
+                }
+                return `${primary} hover:${primaryDark} active:${primaryDark} focus:${primaryDark}`;
+            },
+            /**
+             * icon class object
+             */
+            iconClass() {
+                return CssClasses.icon;
+            },
         },
         methods: {
             /**
