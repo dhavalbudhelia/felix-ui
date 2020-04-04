a714c6f61141a994a57cfe1f55948f217fef1626
Dhaval Budhelia <dhaval@supportability.com.au>
2020-04-04 23:39:18 +1000

    Dropdown: moving scss style to the component

---
+++ src/components/Dropdown/CssClasses.js
@@ -0,0 +1,13 @@
+const cssClasses = {
+    base: 'block align-top',
+    menu:'block z-20 pt-1',
+    content: 'bg-white rounded border border-gray-400 shadow py-2',
+    item: 'text-gray-800 block leading-normal py-1 px-4 relative hover:bg-gray-200 hover:cursor-pointer',
+    backdrop: 'fixed w-full h-full top-0 left-0 z-10 md:bg-white md:opacity-0 bg-black opacity-75',
+    sizeXs: 'text-xs',
+    sizeSm: 'text-sm',
+    sizeMd: 'text-base',
+    sizeLg: 'text-lg',
+};
+
+export default cssClasses;
